d4bd072ec366e1ff474a74c5d046122a6b865ba8
Nela <museln00@jcu.cz>
2025-02-26 23:10:23 +0000

    sude_nebo_liche

--- first.py
+++ first.py
@@ -1,1 +1,15 @@
-print("Hello World!");
+# Definice funkce s podmínkou if
+def sudy_nebo_lichy(cislo):
+    if cislo % 2 == 0:
+        SudeLiche = "sudé"
+    else:
+        SudeLiche = "liché"
+    return f"Číslo", cislo, "je", SudeLiche, "."
+
+# Volání funkce a ukládání návratových hodnot do proměnných
+veta1 = sudy_nebo_lichy(5)
+veta2 = sudy_nebo_lichy(1000000)
+
+# Výstup pomocí print()
+print(veta1)
+print(veta2)
